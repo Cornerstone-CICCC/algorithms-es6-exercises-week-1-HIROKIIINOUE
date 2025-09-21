4e8cbea0090f247c44982ffd347d8e8034895f73
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-21 16:45:40 -0700

    exercise 28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,36 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  yourName: "Hiroki",
+  numberOfYourFriends: 8,
+  yourPost: ["Hello", "Thank you", "Good bye"],
+  post: function postMessage(message) {
+    this.yourPost.push(message);
+  },
+  delete: function deleteMessage(index) {
+    this.yourPost.splice(index, 1);
+  },
+  add: function addFriend() {
+    this.numberOfYourFriends++;
+  },
+  remove: function removeFriend() {
+    this.numberOfYourFriends--;
+  },
+};
+
+console.log(facebookProfile);
+
+//test
+facebookProfile.post("added");
+console.log("add post", facebookProfile);
+
+facebookProfile.delete(0);
+console.log("delete post", facebookProfile);
+
+facebookProfile.add();
+console.log("add friend", facebookProfile);
+
+facebookProfile.remove();
+console.log("remove friend", facebookProfile);
